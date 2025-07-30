0a56f5193087146478edf1c8efcb561fa68aedce
Bladimir Ortez <bladimir7ortez@gmail.com>
2025-07-30 08:18:56 -0700

    Aplica solución final para chats y añade mejoras

--- src/App.js
+++ src/App.js
@@ -187,7 +187,7 @@ export default function App() {
                 };
 
                 await setDoc(chatRef, {
-                    participants: [currentUserAuth.uid, chatInfo.recipientId],
+                    participants: [currentUserAuth.uid, chatInfo.recipientId].sort(),
                     participantInfo: {
                         [currentUserAuth.uid]: currentUserData,
                         [chatInfo.recipientId]: recipientData
